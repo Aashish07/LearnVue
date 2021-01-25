27c40002655682d25340b2d1fda78fd1cc6f13f9
Aashish07 <aashishpasricha7@gmail.com>
2021-01-25 17:42:49 +0530

    Added one and two way data binding

---
+++ blog/src/components/Child.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <h1>Child Component </h1>
+        <!-- <h3>{{name}}</h3> -->
+        <h3 v-for="user in users" :key="user.name">
+            {{user.name}} -- {{user.email}}
+        </h3>
+    </div>
+</template>
+<script>
+export default {
+    name: "Child",
+    props: [
+        'users'
+    ]
+}
+</script>
---
+++ blog/src/components/DataBind.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <h1>Bind</h1>
+        <h1 style="white-space:pre-line">{{message}}</h1> <!-- for next line inclusion-->
+        <!-- <input type="text" v-model="message" >   1 way data binding -->
+        <!--  for next line input, use textarea-->
+        <textarea type="text" v-model="message" />
+    </div>
+</template>
+<script>
+export default {
+    name: "DataBind",
+    data() {  // defining property
+    return {
+        message : "Hi"
+    }
+    }
+}
+</script>
